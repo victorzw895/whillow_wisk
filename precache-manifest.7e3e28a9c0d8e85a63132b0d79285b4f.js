d9ca8493568c701716b816d13c4cd777dc1e01ab
Victor Zhang <victorzw895@gmail.com>
2020-01-05 21:49:53 +1100

    Updates

---
+++ precache-manifest.7e3e28a9c0d8e85a63132b0d79285b4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06a537efc50d95a427a22957723f210f",
+    "url": "/whillow-wisk/index.html"
+  },
+  {
+    "revision": "af2561172dbf6788f371",
+    "url": "/whillow-wisk/static/css/main.c82f678d.chunk.css"
+  },
+  {
+    "revision": "86a01b6adbc7ad0f15c1",
+    "url": "/whillow-wisk/static/js/2.7300d073.chunk.js"
+  },
+  {
+    "revision": "af2561172dbf6788f371",
+    "url": "/whillow-wisk/static/js/main.a5651957.chunk.js"
+  },
+  {
+    "revision": "9c14764f7d88e344f7a5",
+    "url": "/whillow-wisk/static/js/runtime-main.6607d374.js"
+  }
+]);
